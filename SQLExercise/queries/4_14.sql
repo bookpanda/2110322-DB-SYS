bf2e38eae13416ac2ac9f52695e5655deb09db85
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-10 16:38:36 +0700

    14

---
+++ SQLExercise/queries/4_14.sql
@@ -0,0 +1,16 @@
+INSERT INTO product(
+        product_id,
+        product_description,
+        product_finish,
+        standard_price
+    )
+VALUES (7, 'kitchen cabinet', 'Cherry', 1500.00);
+INSERT INTO product(
+        product_id,
+        product_description,
+        product_finish,
+        standard_price
+    )
+VALUES (8, 'table', 'Red Oak', 550.00);
+SELECT *
+FROM product;
